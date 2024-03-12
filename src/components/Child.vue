8ea832236cfe6acd6ad3c50fdc946d5ed5b16802
CCBaxter84 <troyceclark@gmail.com>
2024-03-11 22:08:59 -0600

    Created parent & child

---
+++ src/components/Child.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h3>Child</h3>
+    <button @click="incrementCount"
+            class="button">
+      Click Me
+    </button>
+    <article class="is-flex">
+      <p>Count: </p>
+      <p>{{ count }}</p>
+    </article>
+  </div>
+</template>
+
+<script setup>
+// const props = defineProps({
+//   count: Number
+// })
+
+// function incrementCount() {
+//   props.count++
+// }
+
+// const props = defineProps({
+//   count: Number
+// })
+// const emit = defineEmits(['update:count'])
+
+// function incrementCount() {
+//   emit('update:count', props.count + 1)
+// }
+
+let count = defineModel('count')
+
+function incrementCount() {
+  count.value++
+}
+</script>
---
+++ src/components/Parent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Parent</h2>
+    <article class="is-flex">
+      <p>Count: </p>
+      <p>{{ count }}</p>
+    </article>
+    <br>
+    <Child v-model:count="count"/>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue"
+import Child from "./Child.vue"
+
+let count = ref(0)
+</script>
