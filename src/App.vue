a9bb80a209134d2c7c4ce4f0437bfba983ef92c2
CCBaxter84 <troyceclark@gmail.com>
2024-03-14 07:27:46 -0600

    Updated Vue logo & added styling

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
+  <header style="background-color: aliceblue; border-radius: 8px;">
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </header>
   <main>
     <Parent />
   </main>
@@ -7,3 +12,18 @@
 <script setup>
 import Parent from "./components/Parent.vue"
 </script>
+
+<style scoped>
+.logo {
+  height: 5em;
+  padding: 1em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.react:hover {
+  filter: drop-shadow(0 0 2em #61dafbaa);
+}
+</style>
